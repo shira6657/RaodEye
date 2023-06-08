2046e992ba8e6c5788780241adf370af9352af3e
shira gold <shira6657@gmail.com>
2023-06-08 10:30:58 +0300

    add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,66 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { getTasks, sendVideo } from "./lib/apiClient";
-import TaskForm from "./screens/TaskForm";
-import TasksView from "./screens/TasksView";
-import Login from "./components/Login";
+import UploadVideo from "./screens/uploadVideo";
+import BasicTable from "./screens/BasicTable";
 //import VideoUploader from "./components/VideoUploader";
 
 export const DataContext = React.createContext({});
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+   
 
-  useEffect(() => {
-    getTasks().then((tasksFromServer) => {
-      console.log(tasksFromServer);
-      setTasks(tasksFromServer);
-    });
-  }, []);
-  
-  let students = [{id:5635127,color:"jhdjs"},{id:345678,color:"sdfghjk"}]
-
-  const handleVideoUpload = (event) => {
-    const file = event.target.files[0];
-    sendVideo(file);
-    // Do something with the video file
-    console.log("Uploaded video:", file);
-  };
 
   return (
     <>
     <DataContext.Provider
       value={{
-        tasks: tasks,
       }}
     >
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/tasks" element={<TasksView />} />
-          <Route path="/task-form/:id?" element={<TaskForm />} />
+          <Route exact path="/" element={<UploadVideo/>} />
+          <Route exact path="/table" element={<BasicTable/>} />
         </Routes>
-        <div>
-          <label htmlFor="label-video-upload-input">Upload Video:</label>
-          <input
-            type="file"
-            id="video-upload-input"
-            accept="video/*"
-            style={{ display: "none" }}
-            onChange={handleVideoUpload}
-          />
-          <button
-            onClick={() => {
-              document.getElementById("video-upload-input").click();
-            }}
-          >
-            Select Video
-          </button>
-        </div>
+        
       </BrowserRouter>
     </DataContext.Provider>
-  <TaskForm arr={students}/>
   </>
   );
 };
--- frontend/src/screens/BasicTable.jsx
+++ frontend/src/screens/BasicTable.jsx
@@ -6,6 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 
 
@@ -24,9 +25,16 @@ const rows = [
   createData('Gingerbread', 356),
 ];
 
-export default function BasicTable({arr}) {
+export default function BasicTable() {
+  const params = useLocation()
+  let navigate = useNavigate();
+
+  const arr =  params.state.arr;
   return (
     <>  
+    <button onClick={()=>{
+      navigate('/');
+    }}>back</button>
     <TableContainer component={Paper} sx={{width:'80vw',margin:"10vw"}}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead sx={{backgroundColor:"lightgreen"}}>
