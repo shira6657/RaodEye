876b865566ef79b348e07a3b3273b822c938515d
shira gold <shira6657@gmail.com>
2023-06-08 17:22:38 +0300

    send video workss yayyy

--- frontend/src/screens/uploadVideo.jsx
+++ frontend/src/screens/uploadVideo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {  sendVideo } from "../lib/apiClient";
+import { sendVideo } from "../lib/apiClient";
 import { useNavigate } from 'react-router-dom';
 
 //import VideoUploader from "./components/VideoUploader";
@@ -7,53 +7,45 @@ import { useNavigate } from 'react-router-dom';
 export const DataContext = React.createContext({});
 
 function UploadVideo() {
-    
-    let navigate = useNavigate();
-  
-  const handleVideoUpload = (event) => {
-    const file = event.target.files[0];
-    console.log('file: ', file);
-    sendVideo(file);
-    let students = [{id:5635127,color:"jhdjs"},{id:345678,color:"sdfghjk"}]
 
-    navigate('table' , {state: {arr:students}});
-    // Do something with the video file
-    console.log("Uploaded video:", file);
+  let navigate = useNavigate();
+
+  const submit = () => {
+    var file = document.getElementById("fileInput").files[0];
+    if (file) {
+      var filereader = new FileReader();
+      filereader.readAsDataURL(file);
+      filereader.onload = function (evt) {
+        var base64 = evt.target.result;
+        sendVideo(base64);
+      }
+      let students = [{ id: 5635127, color: "jhdjs" }, { id: 345678, color: "sdfghjk" }]
+
+      navigate('table', { state: { arr: students } });
+      // Do something with the video file
+    };
+  }
+
+    return (
+      <>
+        <DataContext.Provider
+          value={{
+
+          }}
+        >
+          <div>
+            <label htmlFor="label-video-upload-input">Upload Video:</label>
+            <input type="file" id="fileInput" name="file" multiple onChange={() => submit()} />
+          </div>
+
+        </DataContext.Provider>
+
+      </>
+    );
   };
 
-  return (
-    <>
-    <DataContext.Provider
-      value={{
-       
-      }}
-    >
-        <div>
-          <label htmlFor="label-video-upload-input">Upload Video:</label>
-          <input
-            type="file"
-            id="video-upload-input"
-            accept="video/*"
-            style={{ display: "none" }}
-            onChange={handleVideoUpload}
-          />
-          <button
-            onClick={() => {
-              document.getElementById("video-upload-input").click();
-            }}
-          >
-            Select Video
-          </button>
-        </div>
- 
-    </DataContext.Provider>
- 
-  </>
-  );
-};
-
-//export default StudentTable;
+  //export default StudentTable;
 
 
 
-export default UploadVideo;
+  export default UploadVideo;
