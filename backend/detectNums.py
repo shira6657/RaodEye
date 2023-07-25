22036ebe8da056be425b7343966e3e00b2175304
shira gold <shira6657@gmail.com>
2023-07-25 16:00:33 +0300

    change access token

--- backend/detectNums.py
+++ backend/detectNums.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 #258dc07d366deaf974103c79726b3803f83c2ce2
+#ee39ff412322e1c2a298caf2327b71e10cf50ad0
 def detect_Num(img):
     regions = ['mx', 'us-ca'] # Change to your country
     with open(img, 'rb') as fp:
@@ -9,7 +10,7 @@ def detect_Num(img):
                 'https://api.platerecognizer.com/v1/plate-reader/',
                 data=dict(regions=regions, mmc=True),  # Optional
                 files=dict(upload=fp),
-                headers={'Authorization': 'Token ee39ff412322e1c2a298caf2327b71e10cf50ad0'})
+                headers={'Authorization': 'Token 30e0ef5cf7b8fdeec83993550f95fc5970ccbc50'})
         except :
             print("err")
             return []
